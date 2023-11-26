64bc3523e64dd7e4bdccdc506c6c353eee4d7759
Euphoria99 <363pavanbhat@gmail.com>
2023-11-26 17:25:09 +0530

    tailwind theming and font setup

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>
-      <h3>Works</h3>
-    </div>
+    <main>
+      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit fugit voluptatem eveniet velit quam corrupti officia ipsam ducimus exercitationem. Expedita, quia. Iusto repellendus maxime repudiandae animi excepturi, dolorem asperiores amet!</div>
+    </main>
   )
 }
