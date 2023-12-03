5830f1f25f16909d4dff53af57cbe2d18aa8776f
Euphoria99 <363pavanbhat@gmail.com>
2023-12-03 21:58:02 +0530

    Experience section

---
+++ components/main/Experience.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import SectionTitle from '../sub-components/SectionTitle'
+import GB1 from '../sub-components/work/GB1'
+import GB2 from '../sub-components/work/GB2'
+
+const Experience = () => {
+
+  const [workGB1, setWorkGB1] = useState(true)
+  const [workGB12, setWorkGB12] = useState(false)
+
+  const handleGB1 = () => {
+    setWorkGB1(true)
+    setWorkGB12(false)
+  }
+
+  const handleGB12 = () => {
+    setWorkGB1(false)
+    setWorkGB12(true)
+  }
+  
+  return (
+    <section 
+    id="section"
+    className='max-w-containerxs mx-auto py-10 lgl:py-24 px-4'
+    >
+              <SectionTitle title="Experience" titleNo="02" />
+              <div className='w-full mt-10 flex flex-col md:flex-row gap-16'>
+                    <ul className='md:w-32 flex flex-col'>
+                        <li 
+                        onClick={handleGB1} 
+                        className={
+                          `${workGB1 
+                          ? "border-l-textColor text-textColor" 
+                          : "border-l-hoverColor text-textDark"} border-l-2  bg-transparent hover:bg-[#112240] py-3 text-sm cursor-pointer duration-300 px-8 font-medium`}>
+                            GB1
+                        </li>
+                        <li 
+                        onClick={handleGB12} 
+                        className={
+                          `${workGB12 
+                          ? "border-l-textColor text-textColor" 
+                          : "border-l-hoverColor text-textDark"} border-l-2  bg-transparent hover:bg-[#112240] py-3 text-sm cursor-pointer duration-300 px-8 font-medium`}>
+                            GB12
+                        </li>
+                    </ul>
+                    { workGB1 && <GB1 />}
+                    { workGB12 && <GB2 />}
+                    
+              </div>
+    </section>
+  )
+}
+
+export default Experience
---
+++ components/sub-components/work/GB1.tsx
@@ -0,0 +1,40 @@
+'use-client'
+
+import { motion } from 'framer-motion';
+import { AiTwotoneThunderbolt } from "react-icons/ai";
+
+const GB1 = () => {
+  return (
+    <motion.div
+    initial={{opacity: 0}}
+    animate={{opacity: 1}}
+    transition={{ delay: 0.1 }}
+    className='w-full'
+    >
+        <h3 className='flex gap-1 font-medium text-xl font-titleFont'>Web Developer <span className='text-textColor tracking-wide'>@GB1</span></h3>
+        <p className='text=sm mt-1 font-medium text-textDark'>Aug 2022 - present</p>
+        <ul className='mt-6 flex flex-col gap-3'>
+            <li className='text-base flex gap-2 text-textDark '>
+                <span className='text-textColor mt-1'>
+                    <AiTwotoneThunderbolt />
+                </span>
+                Write modern, performant, maintainable code for a diverse array of client and internal projects
+            </li>
+            <li className='text-base flex gap-2 text-textDark '>
+                <span className='text-textColor mt-1'>
+                    <AiTwotoneThunderbolt />
+                </span>
+                Work with a variety of different languages, platforms, frameworks, and content management systems such as JavaScript, TypeScript, Gatsby, React, Craft, WordPress, Prismic, and Netlify
+            </li>
+            <li className='text-base flex gap-2 text-textDark '>
+                <span className='text-textColor mt-1'>
+                    <AiTwotoneThunderbolt />
+                </span>
+                Communicate with multi-disciplinary teams of engineers, designers, producers, and clients on a daily basis
+            </li>
+        </ul>
+    </motion.div>
+  )
+}
+
+export default GB1
