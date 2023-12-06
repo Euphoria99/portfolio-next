e6b8dfe93a310622baec4fd3cd34aedb55add4ee
Euphoria99 <363pavanbhat@gmail.com>
2023-12-06 23:16:49 +0530

    Projects component

---
+++ components/main/Projects.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import SectionTitle from '../sub-components/SectionTitle'
+
+const Projects = () => {
+  return (
+    <section id="project" className='max-w-container mx-auto lgl:px-20 py-24'>
+        <SectionTitle title="My Projects" titleNo='03' />
+        {/* Project One */}
+        <div>
+
+        </div>
+        {/* Project Two */}
+        {/* Project Three */}
+    </section>
+  )
+}
+
+export default Projects
