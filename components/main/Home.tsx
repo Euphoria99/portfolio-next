671b56979f4beb6c10ce92e3165fd6a8d50f7a91
Euphoria99 <363pavanbhat@gmail.com>
2023-12-31 20:48:59 +0530

    detecting top of the screen while scrolling

--- components/main/Home.tsx
+++ components/main/Home.tsx
@@ -12,44 +12,52 @@ import Projects from './Projects';
 import Archives from './Archives';
 import Contact from './Contact';
 import Footer from './Footer';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 
 export default function Home() {
+
+  const handleScroll = (e: React.UIEvent<HTMLElement>) => {
+    if(e.currentTarget.scrollTop === 0) {
+       alert("on top")
+    }
+} 
+
   return (
-      <div className="w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll  scrollbar-track-textDark/20 scrollbar-thumb-textDark/60 scrollbar-thin scrollbar-thumb-rounded-full">
-        <Navbar />
-        <div className="w-full h-[88vh] xl:flex items-center gap-20 justify-between">
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ delay: 1.5 }}
-            className='hidden xl:inline-flex w-32 h-full fixed left-0 bottom-0'>
-            <IconPannel />
-          </motion.div>
-          <div className='h-[88vh] w-full mx-auto p-4'>
-            {/*  */}
-            <Banner />
-            {/* About */}
-            <About />
-            {/*Experience  */}
-            <Experience />
-            {/*  Projects*/}
-            <Projects />
-            {/*  Archive*/}
-            <Archives />
-            {/*  Contact*/}
-            <Contact />
-            {/*  Footer*/}
-            <Footer />
-          </div>
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ delay: 1.5 }}
-            className='hidden xl:inline-flex w-32 h-full fixed right-0 bottom-0'>
-            <EmailPannel />
-          </motion.div>
+    <div onScroll={handleScroll} className="w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll scrollbar-track-textDark/20 scrollbar-thumb-textDark/60 scrollbar-thin scrollbar-thumb-rounded-full">
+      <Navbar />
+      <div className="w-full h-[88vh] xl:flex items-center gap-20 justify-between">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 1.5 }}
+          className='hidden xl:inline-flex w-32 h-full fixed left-0 bottom-0'>
+          <IconPannel />
+        </motion.div>
+        <div className='h-[88vh] w-full mx-auto p-4'>
+          {/*  */}
+          <Banner />
+          {/* About */}
+          <About />
+          {/*Experience  */}
+          <Experience />
+          {/*  Projects*/}
+          <Projects />
+          {/*  Archive*/}
+          <Archives />
+          {/*  Contact*/}
+          <Contact />
+          {/*  Footer*/}
+          <Footer />
         </div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 1.5 }}
+          className='hidden xl:inline-flex w-32 h-full fixed right-0 bottom-0'>
+          <EmailPannel />
+        </motion.div>
       </div>
-  )
+    </div>
+  );
 }
