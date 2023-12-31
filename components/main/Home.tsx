052ea95deab10bb47311e5d315faffe18f3c2241
Euphoria99 <363pavanbhat@gmail.com>
2023-12-31 23:20:05 +0530

    Signature and other tweeks

--- components/main/Home.tsx
+++ components/main/Home.tsx
@@ -13,12 +13,16 @@ import Archives from './Archives';
 import Contact from './Contact';
 import Footer from './Footer';
 import { useCallback, useEffect, useRef, useState } from 'react';
+import Signature from '../sub-components/Signature';
+
 
 
 export default function Home() {
 
   const [isAtTop, setIsAtTop] = useState(true)
+  const [homeView, setHomeView] = useState(true)
   const handleScroll = (e: React.UIEvent<HTMLElement>) => {
+    setHomeView(false);
     setIsAtTop(false);
     if(e.currentTarget.scrollTop === 0) {
       setIsAtTop(true)
@@ -27,7 +31,9 @@ export default function Home() {
 
   return (
     <div onScroll={handleScroll} className="w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll scrollbar-track-textDark/20 scrollbar-thumb-textDark/60 scrollbar-thin scrollbar-thumb-rounded-full">
-      <Navbar isAtTop={isAtTop} />
+      {
+        !homeView && (<Navbar isAtTop={isAtTop} />)
+      } 
       <div className="w-full h-[88vh] xl:flex items-center gap-20 justify-between">
         <motion.div
           initial={{ opacity: 0 }}
@@ -36,9 +42,11 @@ export default function Home() {
           className='hidden xl:inline-flex w-32 h-full fixed left-0 bottom-0'>
           <IconPannel />
         </motion.div>
-        <div className='h-[88vh] w-full mx-auto p-4'>
+        {/* use pt-4 if you are not using signature below */}
+        <div className='h-[88vh] w-full mx-auto '>
           {/*  */}
-          <Banner />
+          {/* <Banner /> */}
+          <Signature />
           {/* About */}
           <About />
           {/*Experience  */}
