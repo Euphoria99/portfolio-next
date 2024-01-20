2eb9a1553c8bf73e0e940c04b6254f2e39db67bc
Euphoria99 <363pavanbhat@gmail.com>
2024-01-20 19:41:59 +0530

    Fixed HTML entities must be escaped

--- components/main/Contacts.tsx
+++ components/main/Contacts.tsx
@@ -7,11 +7,11 @@ const Contact = () => {
     return (
         <section id="contact" className='max-w-contentContainer mx-auto py-10 xl:py-32 flex flex-col gap-4 items-center justify-center'>
         <p className='font-titleFont text-lg font-semibold flex items-center tracking-wide'>
-        <span className='text-base md:text-lg text-textColor mr-2'>04.</span> What's Next?
+        <span className='text-base md:text-lg text-textColor mr-2'>04.</span> What&apos;s Next?
         </p>
         <h2 className='font-titleFont text-5xl font-semibold'>Get In Touch</h2>
         <p className='max-w-[600px] text-center text-textDark'>
-        Although I'm not currently looking for any new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I'll try my best to get back to you!
+        Although I&apos;m not currently looking for any new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I&apos;ll try my best to get back to you!
         </p>
         <a href={`mailto:${bioData.email}`}>
         <button  className='w-28 h-12 text-sm font-titleFont border border-textColor rounded-md text-textColor tracking-wide hover:bg-hoverColor duration-300' >Say Hello</button> 
