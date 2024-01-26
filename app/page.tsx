7bf77c0d179f197713909b06d529319f7adaf25d
Euphoria99 <363pavanbhat@gmail.com>
2024-01-27 00:02:35 +0530

    Added metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,41 @@
 import Home from "@/components/main/Home";
 
 
+export const metadata = {
+  generator: 'Next.js',
+  applicationName: 'Pavan Bhats Portfolio',
+  referrer: 'origin-when-cross-origin',
+  keywords: ['Next.js', 'React', 'JavaScript'],
+  authors: [{ name: 'Pavan' }],
+  creator: 'Pavan Bhat',
+  publisher: 'Pavan Bhat',
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+  openGraph: {
+    type: 'website',
+    title: 'Pavan Bhats Portfolio',
+    description: 'Next.js portfolio by Pavan Bhat',
+    url: '',
+    siteName: 'Pavan Bhat',
+    images: [],
+    locale: 'en_US',
+  },
+  twitter: {
+    card: 'summary_large-image',
+    title: 'Pavan Bhat',
+    description: 'Next.js portfolio by Pavan Bhat',
+    creator: '@pavanbhat_99',
+    image: [
+      {
+        url: '',
+        alt: '',
+      },
+    ],
+  },
+}
 
 
 export default function Page() {
