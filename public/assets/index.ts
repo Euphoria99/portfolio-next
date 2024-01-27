af99e6565880cb41495fc0fc4803f6ff08ecf151
Euphoria99 <363pavanbhat@gmail.com>
2024-01-27 21:43:04 +0530

    Updated metadata-9

--- public/assets/index.ts
+++ public/assets/index.ts
@@ -1,5 +1,5 @@
 import zhang from "./zhang.jpg"
-import zhang2 from "./zhangJ.jpeg"
+import zhang2 from "./zhangTw.jpeg"
 import chatApp from "./chat-app.jpg"
 import nextLogo from "./nextLogo.svg"
 
