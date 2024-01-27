e3ceebfdce719712c88855dc01799ab9c570a65d
Euphoria99 <363pavanbhat@gmail.com>
2024-01-27 21:03:31 +0530

    Updated metadata-3

--- app/page.tsx
+++ app/page.tsx
@@ -33,12 +33,7 @@ export const metadata: Metadata = {
     title: 'Pavan Bhat',
     description: 'Pavan Bhat\'s Portfolio',
     creator: '@pavanbhat_99',
-    images: [
-      {
-        url: zhang.src,
-        alt: 'Pavan\'s Image',
-      },
-    ],
+    images: zhang.src
   },
   // 
 };
